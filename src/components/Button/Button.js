6b3088e94b250b738e264bdd2bf4fa24a6bee1de
ykji <yvarshney44@gmail.com>
2023-09-19 17:35:09 +0530

    feat: add Button story

---
+++ src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Button.css";
+
+const Button = (props) => {
+  const { variant = "primary", children, ...rest } = props;
+
+  return (
+    <button className={`button ${variant}`} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;

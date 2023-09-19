99eda510a1b332155b10f242beb6ef66d2efa579
ykji <yvarshney44@gmail.com>
2023-09-19 17:52:59 +0530

    feat: add Input story

---
+++ src/components/Input/Input.stories.js
@@ -0,0 +1,10 @@
+import Input from "./Input";
+
+export default {
+  title: "form/Input",
+  component: Input,
+};
+
+export const Small = () => <Input size="small" placeholder="Small size" />;
+export const Medium = () => <Input size="medium" placeholder="Medium size" />;
+export const Large = () => <Input size="large" placeholder="Large size" />;

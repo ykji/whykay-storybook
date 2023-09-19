fb25303ce73ed5fc6d03d606ac05fe0257b50b92
ykji <yvarshney44@gmail.com>
2023-09-19 17:53:26 +0530

    feat: sort stories alphabetically

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,12 @@
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    options: {
+      storySort: (a, b) =>
+        a.id === b.id
+          ? 0
+          : a.id.localeCompare(b.id, undefined, { numeric: true }),
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
